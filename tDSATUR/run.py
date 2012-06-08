90d9c99d5a9225b29bad4aa781719b59ca7d4258
andrewju <andrew.ju@ul.ie>
2012-06-08 20:52:55 +0100

    exact DSATUR

--- tDSATUR/run.py
+++
@@ -1,9 +0,0 @@
-import os, platform, datetime
-logfile = open(datetime.datetime.now().strftime('%Y-%m-%d-%H:%M:%S') + '.log', 'w')
-for item in os.listdir('/home/andrew/Dropbox/Debian/code/instances/'):
-	name = os.path.join('/home/andrew/Dropbox/Debian/code/instances/', item)
-	handle = os.popen('./m 1 '+name)
-	log = item.ljust(20) + handle.read().strip().rjust(55)
-	print log
-	logfile.write(log + '\n')
-logfile.close()
